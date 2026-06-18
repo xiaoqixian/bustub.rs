e1df2d2f2162423aba94911edce2521b570405fb
lunar <2100125584@qq.com>
2026-06-18 12:02:50 +0800

    add BustubError

---
+++ src/common/errors.rs
@@ -0,0 +1,18 @@
+use crate::binder::BindError;
+
+pub enum BustubError {
+    BindError(BindError),
+    Message(String),
+}
+
+impl From<BindError> for BustubError {
+    fn from(e: BindError) -> Self {
+        Self::BindError(e)
+    }
+}
+
+impl From<String> for BustubError {
+    fn from(e: String) -> Self {
+        Self::Message(e)
+    }
+}
