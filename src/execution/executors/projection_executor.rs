c8c835d8db0c2ac4768bc47cf3a9d6ae301de988
lunar <lunar_ubuntu@qq.com>
2026-06-26 23:28:07 +0800

    add projection executor impl

--- src/execution/executors/projection_executor.rs
+++ src/execution/executors/projection_executor.rs
@@ -31,6 +31,11 @@ pub struct ProjectionExecutor<'a> {
     exec_ctx: &'a ExecutorContext,
     plan: &'a ProjectionPlanNode,
     child_executor: Box<dyn Executor + 'a>,
+    // child tuple batch offset
+    child_offset: usize,
+    // reusable child tuple batch & rid batch
+    child_tuples: Vec<Tuple>,
+    child_rids: Vec<RID>,
 }
 
 impl<'a> ProjectionExecutor<'a> {
@@ -43,13 +48,47 @@ impl<'a> ProjectionExecutor<'a> {
             exec_ctx,
             plan,
             child_executor,
+            child_offset: 0,
+            child_tuples: Vec::new(),
+            child_rids: Vec::new(),
         }
     }
 }
 
 impl<'a> Executor for ProjectionExecutor<'a> {
-    fn next(&mut self, _batch_size: usize) -> Option<(Vec<Tuple>, Vec<RID>)> {
-        todo!("")
+    fn next(&mut self, batch_size: usize) -> Option<(Vec<Tuple>, Vec<RID>)> {
+        let mut tuple_batch = Vec::with_capacity(batch_size);
+        let mut rid_batch = Vec::with_capacity(batch_size);
+
+        while tuple_batch.len() < batch_size {
+            for (i, (child_tuple, child_rid)) in self.child_tuples.iter().zip(self.child_rids.iter()).enumerate().skip(self.child_offset) {
+                let child_schema = self.child_executor.output_schema_ref();
+                let values = self.plan.expressions.iter()
+                    .map(|expr| expr.evaluate(child_tuple, child_schema))
+                    .collect::<Vec<_>>();
+                
+                tuple_batch.push(Tuple::new_with_values(values, &self.plan.output_schema));
+                rid_batch.push(*child_rid);
+
+                if tuple_batch.len() == batch_size {
+                    self.child_offset = i;
+                    break;
+                }
+            }
+            
+            if tuple_batch.len() < batch_size {
+                (self.child_tuples, self.child_rids) = match self.child_executor.next(batch_size) {
+                    None => break,
+                    Some(x) => x,
+                };
+                self.child_offset = 0;
+            }
+        }
+
+        match tuple_batch.is_empty() {
+            true => None,
+            false => Some((tuple_batch, rid_batch))
+        }
     }
 
     fn output_schema_ref(&self) -> &Schema {
